1baf4f39b5d7856729f3f1f115cf8641049334bb
ol4ash <shaukavets@gmail.com>
2018-09-19 22:43:56 +0300

    task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+str = str.split("");
+config = bracketsConfig;
+
+for (var y=0; y<str.length-1; y++) {
+  var z = y+1;
+  var arrayTest = [str[y],str[z]];
+
+for (var i=0; i<config.length; i++){
+  arrayCheck = config[i];
+
+if ((arrayTest[0]===arrayCheck[0])&&(arrayTest[1]===arrayCheck[1])){
+  str.splice(y,2);
+  y=-1;
+}
+}
+}
+
+if (str.length ===0){
+	result = true;
+}
+  else {
+  	result = false;
+  }
+
+
+return result
+
 }
